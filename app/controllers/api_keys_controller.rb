8d1483041778545455115dddfa79774e3b67fa62
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-11-20 01:41:21 +0900

    add api_key endpoint to controller

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -1,4 +1,26 @@
 class ApiKeysController < ApplicationController
+    def show
+        @api_key = ApiKey.find(params[:id])
+        render json: @api_key
+    end
+
+    def create
+        @api_key = ApiKey.new(api_key_params)
+        if @api_key.save
+            render json: @api_key, status: created
+        else
+            render json: @api_key.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @api_key = ApiKey.find(params[:id])
+        if @api_key.update(api_key_params)
+            render json: @api_key
+        else
+            render json: @api_key.errors, status: unproccesable_entity
+        end
+    end
 
     private
 
